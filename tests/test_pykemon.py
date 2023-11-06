eb9e249ece9936d8f4a94727497960fd4cb5d2c2
AlvisYan2025 <hy2275@nyu.edu>
2023-11-05 19:09:52 -0500

    changed some strucure, add init

--- tests/test_pykemon.py
+++ tests/test_pykemon.py
@@ -1,12 +1,11 @@
 import pytest
 from src import pykemon
+from src.ascii_art_pykemon import pokemons
 class Tests:
-  def test_sanity_check(self, example_fixture):
-    """
-    Test debugging... making sure that we can run a simple test that always passes.
-    Note the use of the example_fixture in the parameter list - any setup and teardown in that fixture will be run before and after this test function executes
-    From the main project directory, run the `python3 -m pytest` command to run all tests.
-    """
-    expected = True # the value we expect to be present
-    actual = True # the value we see in reality
-    assert actual == expected, "Expected True to be equal to True!"
+    def test_sanity_check(self):
+        expected = True 
+        actual = True 
+        assert actual == expected, "Test Sanity Check Complete"
+    def test_p_num(self):
+        pass
+
